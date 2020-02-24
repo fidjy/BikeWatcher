469dc97ca077cd0c90ba9179b7e1947b8f03a9a8
Quentin FAYET <44276782+fidjy@users.noreply.github.com>
2020-02-24 15:58:27 +0100

    initialize StationControler

---
+++ BikeWatcher/Controllers/StationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace BikeWatcher.Controllers
+{
+    public class APIController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<IActionResult> Index()
+        {
+            var stations = await ProcessRepositories();
+            ViewBag.allStations = stations.OrderBy(s => s.name);
+            return View();
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<RootObject>(await streamTask);
+            return repositories.values;
+        }
+    }
+}
